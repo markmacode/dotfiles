809098d1e74ff8fc362be1c8b35c29d19b3d5a14
mark-bromell <markbromell.business@gmail.com>
2021-05-26 18:15:51 +0100

    Added boilerplate project creation stuff

---
+++ create/python/main.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+create_python() {
+    project_name=$1;
+    git init $project_name;
+    cd $project_name;
+    git branch -M main
+    cp -a ~/create/python/template/. .;
+    mv "./#project_name" ./$project_name;
+
+    # Create the virtual environment.
+    _os="$(uname)";
+    if [[ $_os =~ "MINGW64_NT" ]]; then
+        python -m venv ./venv-mingw;
+        source ./venv-mingw/Scripts/activate;
+    elif [[ $_os =~ "Linux" ]]; then
+        python3 -m venv ./venv-linux;
+        source ./venv-linux/bin/activate;
+    else
+        echo "ERROR: could not make venv";
+    fi;
+
+    pip install -r requirements.txt;
+}
