152d4dc290bad9b2649633f8da94b39753bccfb1
mbromell <code@mamo.aleeas.com>
2023-12-29 13:06:02 +0000

    Update

--- home/wezterm/.config/wezterm/wezterm.lua
+++ home/wezterm/.config/wezterm/wezterm.lua
@@ -8,8 +8,6 @@ if wezterm.config_builder then
     config = wezterm.config_builder()
 end
 
--- ===>
-
 -- This is where you actually apply your config choices
 config.color_scheme = 'nightfox'
 config.hide_tab_bar_if_only_one_tab = true
@@ -18,5 +16,54 @@ config.font = wezterm.font {
     harfbuzz_features = { 'ss02', 'ss19', 'cv18' }
 }
 config.font_size = 14
+
+-- Key overrides
+config.keys = {
+    {
+        key = 'LeftArrow',
+        mods = 'ALT|SHIFT',
+        action = wezterm.action.SendKey { key = '!', mods = 'ALT' },
+    },
+    {
+        key = 'DownArrow',
+        mods = 'ALT|SHIFT',
+        action = wezterm.action.SendKey { key = '@', mods = 'ALT' },
+    },
+    {
+        key = 'UpArrow',
+        mods = 'ALT|SHIFT',
+        action = wezterm.action.SendKey { key = '#', mods = 'ALT' },
+    },
+    {
+        key = 'RightArrow',
+        mods = 'ALT|SHIFT',
+        action = wezterm.action.SendKey { key = '$', mods = 'ALT' },
+    },
+    {
+        key = 'LeftArrow',
+        mods = 'ALT|SUPER',
+        action = wezterm.action.SendKey { key = '%', mods = 'ALT' },
+    },
+    {
+        key = 'DownArrow',
+        mods = 'ALT|SUPER',
+        action = wezterm.action.SendKey { key = '^', mods = 'ALT' },
+    },
+    {
+        key = 'UpArrow',
+        mods = 'ALT|SUPER',
+        action = wezterm.action.SendKey { key = '&', mods = 'ALT' },
+    },
+    {
+        key = 'RightArrow',
+        mods = 'ALT|SUPER',
+        action = wezterm.action.SendKey { key = '*', mods = 'ALT' },
+    },
+    {
+        key = ']',
+        mods = 'ALT|SHIFT',
+        action = wezterm.action.SendKey { key = '[', mods = 'ALT' },
+    },
+}
 
 return config
