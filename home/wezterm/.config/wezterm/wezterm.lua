77beab99e0d9d98080e1d2e5201fd93bd9f8f940
mbromell <code@mamo.aleeas.com>
2024-01-15 18:47:40 +0000

    Organised wezterm config

--- home/wezterm/.config/wezterm/wezterm.lua
+++ home/wezterm/.config/wezterm/wezterm.lua
@@ -1,72 +1,16 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
-local config = {}
-
--- Thank you for the hack mnicky https://stackoverflow.com/a/14425862
-local is_windows = package.config:sub(1,1) == "\\"
-
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-if wezterm.config_builder then
-    config = wezterm.config_builder()
-end
+local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 config.color_scheme = 'nightfox'
+config.keys = require('keymap')
 config.hide_tab_bar_if_only_one_tab = true
 config.font = wezterm.font {
     family = 'JetBrains Mono',
     harfbuzz_features = { 'ss02', 'ss19', 'cv18' }
 }
-if is_windows then config.font_size = 12 else config.font_size = 14 end
+config.font_size = 14
 
--- Key overrides
-config.keys = {
-    {
-        key = 'LeftArrow',
-        mods = 'ALT|SHIFT',
-        action = wezterm.action.SendKey { key = '!', mods = 'ALT' },
-    },
-    {
-        key = 'DownArrow',
-        mods = 'ALT|SHIFT',
-        action = wezterm.action.SendKey { key = '@', mods = 'ALT' },
-    },
-    {
-        key = 'UpArrow',
-        mods = 'ALT|SHIFT',
-        action = wezterm.action.SendKey { key = '#', mods = 'ALT' },
-    },
-    {
-        key = 'RightArrow',
-        mods = 'ALT|SHIFT',
-        action = wezterm.action.SendKey { key = '$', mods = 'ALT' },
-    },
-    {
-        key = 'LeftArrow',
-        mods = 'ALT|SUPER',
-        action = wezterm.action.SendKey { key = '%', mods = 'ALT' },
-    },
-    {
-        key = 'DownArrow',
-        mods = 'ALT|SUPER',
-        action = wezterm.action.SendKey { key = '^', mods = 'ALT' },
-    },
-    {
-        key = 'UpArrow',
-        mods = 'ALT|SUPER',
-        action = wezterm.action.SendKey { key = '&', mods = 'ALT' },
-    },
-    {
-        key = 'RightArrow',
-        mods = 'ALT|SUPER',
-        action = wezterm.action.SendKey { key = '*', mods = 'ALT' },
-    },
-    {
-        key = ']',
-        mods = 'ALT|SHIFT',
-        action = wezterm.action.SendKey { key = '[', mods = 'ALT' },
-    },
-}
-
+require('windows').apply(config)
 return config
---
+++ home/wezterm/.config/wezterm/windows.lua
@@ -0,0 +1,27 @@
+local wezterm = require 'wezterm'
+local module = {}
+
+function module.is_windows()
+    -- Thank you for the hack mnicky https://stackoverflow.com/a/14425862
+    return package.config:sub(1,1) == "\\"
+end
+
+function module.apply(config)
+    if not module.is_windows() then
+        wezterm.log_info 'Cannot apply Windows config (not on Windows)'
+        return
+    end
+    wezterm.log_info 'Applying Windows config'
+
+    config.font_size = 12
+    config.wsl_domains = {
+        {
+            name = 'WSL:Ubuntu',
+            distribution = 'Ubuntu',
+            default_cwd = '~',
+        },
+    }
+    config.default_domain = 'WSL:Ubuntu'
+end
+
+return module
