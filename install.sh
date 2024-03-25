9f48f1fc0f153369960af55de38ad8f18544bd48
mbromell <code@mamo.aleeas.com>
2024-03-25 13:34:27 +0000

    Fixing git config

--- install.sh
+++ install.sh
@@ -15,15 +15,20 @@ fi
 
 # Do Windows stuff and get out of the way
 if [[ "$DOTFILES_OS" == "mingw" ]]; then
+    export MSYS="winsymlinks:nativestrict"
     mkdir -p "$HOME/.config"
+
+    if [[ -d "$DOTFILES/home/intel/.config/work/" ]]; then
+        ln -s -t "$HOME/.config" "$DOTFILES/home/intel/.config/work/"
+        ln -s -t "$HOME" "$HOME/.config/work/.gitconfig"
+        ln -s "$DOTFILES/home/git/.gitconfig" "$HOME/.gitconfig.personal"
+    fi
+
     find "$DOTFILES/home/shell/" -type f -exec ln -s -t "$HOME/" {} \;
     find "$DOTFILES/home/git/" -type f -exec ln -s -t "$HOME/" {} \;
     ln -s -t "$HOME/.config" "$DOTFILES/home/starship/.config/starship.toml"
     ln -s -t "$HOME/.config" "$DOTFILES/home/nvim/.config/nvim/"
     ln -s -t "$HOME/.config" "$DOTFILES/home/mbromell/.config/mbromell/"
-    if [[ -d "$DOTFILES/home/intel/.config/work/" ]]; then
-        ln -s -t "$HOME/.config" "$DOTFILES/home/intel/.config/work/"
-    fi
     exit 0
 fi
 
