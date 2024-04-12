f160fbe21d9c1dde8a36374859ae5d097d60b64a
mbromell <code@mamo.aleeas.com>
2024-04-12 18:19:23 +0100

    Trying a more brute force backup of home before clone

--- .config/mbromell/install.sh
+++ .config/mbromell/install.sh
@@ -16,8 +16,8 @@ else
     mkdir -p home.bak-$(date +"%Y%m%d%H%M%S")
     cp -R .config home.bak-$(date +"%Y%m%d%H%M%S")/.config
     find . -maxdepth 1 -type f | xargs -I{} cp {} home.bak-$(date +"%Y%m%d%H%M%S")/
+    git --git-dir=$HOME/.dot/ --work-tree=$HOME checkout -f
+    echo "Checked out dotfiles"
 fi
 
-git --git-dir=$HOME/.dot/ --work-tree=$HOME checkout
 git --git-dir=$HOME/.dot/ --work-tree=$HOME config status.showUntrackedFiles no
-echo "Checked out dotfiles"
