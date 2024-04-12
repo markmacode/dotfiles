2105341bf28c8458b5a16ea89d60e7d7873b8f45
mbromell <code@mamo.aleeas.com>
2024-04-12 17:32:30 +0100

    No alias in install script

--- .config/mbromell/install.sh
+++ .config/mbromell/install.sh
@@ -5,15 +5,20 @@ if [[ "$1" == "https" ]]; then
 else
     git clone --bare git@github.com:mbromell/dotfiles.git $HOME/.dot
 fi
-function dot {
-    git --git-dir=$HOME/.dot/ --work-tree=$HOME $@
-}
+
+# Try to checkout code, if there's existing files then back them up
+# and try again
 mkdir -p .config-backup
+git --git-dir=$HOME/.dot/ --work-tree=$HOME checkout
 if [ $? = 0 ]; then
     echo "Checked out config.";
 else
     echo "Backing up pre-existing dot files.";
-    dot checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+    git --git-dir=$HOME/.dot/ --work-tree=$HOME checkout 2>&1 \
+        | egrep "\s+\." \
+        | awk {'print $1'} \
+        | xargs -I{} mv {} .config-backup/{}
 fi
-dot checkout
-dot config status.showUntrackedFiles no
+
+git --git-dir=$HOME/.dot/ --work-tree=$HOME checkout
+git --git-dir=$HOME/.dot/ --work-tree=$HOME config status.showUntrackedFiles no
