925cb7aa7f3e8ef4b99e12f09f0c7baf8553a827
mbromell <code@mamo.aleeas.com>
2024-04-14 20:55:09 +0100

    Adding nvim dir to worktree

---
+++ .config/nvim/lua/plugins/colors.lua
@@ -0,0 +1,46 @@
+return {
+  {
+    "f-person/auto-dark-mode.nvim",
+    config = {
+      update_interval = 1000,
+      set_dark_mode = function()
+        vim.api.nvim_set_option("background", "dark")
+        vim.cmd.colorscheme 'nightfox'
+      end,
+      set_light_mode = function()
+        vim.api.nvim_set_option("background", "light")
+        vim.cmd.colorscheme 'dawnfox'
+      end,
+    },
+  },
+  {
+    'EdenEast/nightfox.nvim',
+    priority = 1000,
+    opts = {
+      options = {
+        styles = {
+          comments = "italic",
+          keywords = "bold",
+        },
+      },
+    },
+  },
+  {
+    'folke/tokyonight.nvim',
+    lazy = false,
+    priority = 1000,
+    ops = {},
+  },
+  {
+    'rebelot/kanagawa.nvim',
+    lazy = false,
+    priority = 1000,
+    ops = {},
+  },
+  {
+    'sainnhe/everforest',
+    lazy = false,
+    priority = 1000,
+    ops = {},
+  },
+}
