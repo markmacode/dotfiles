a45f3298ec6568d95f412b359cf5255bb851daf8
mbromell <code@mamo.aleeas.com>
2024-04-14 21:00:26 +0100

    Updating colors

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -9,7 +9,7 @@ return {
       end,
       set_light_mode = function()
         vim.api.nvim_set_option("background", "light")
-        vim.cmd.colorscheme 'dawnfox'
+        vim.cmd.colorscheme 'dayfox'
       end,
     },
   },
