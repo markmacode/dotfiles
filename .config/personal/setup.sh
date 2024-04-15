563964f59ace60c3b1f9b5017180d89968c34c9a
mbromell <code@mamo.aleeas.com>
2024-04-15 16:54:18 +0100

    Fixing sourcing again

--- .config/personal/setup.sh
+++ .config/personal/setup.sh
@@ -1,3 +1,3 @@
-source "$XDG_CONFIG_HOME/personal/exports.sh"
+source "${XDG_CONFIG_HOME:-$HOME/.config}/personal/exports.sh"
 source "$XDG_CONFIG_HOME/personal/aliases.sh"
 source "$XDG_CONFIG_HOME/personal/functions.sh"
