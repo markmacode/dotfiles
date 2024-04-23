3c412fdb257dfbbe15225d3e78dab32819fe96ba
mbromell <code@mamo.aleeas.com>
2024-04-23 14:36:01 +0100

    Stop lazy.nvim from notifying

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,5 +17,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 
 vim.opt.rtp:prepend(lazypath)
-require("lazy").setup("plugins")
+require("lazy").setup("plugins", {
+  change_detection = { enabled = false },
+})
 require("config")
