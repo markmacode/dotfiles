b4650b72ab34d1216f129672c595cda3ae970a78
mbromell <code@mamo.aleeas.com>
2024-04-24 11:33:42 +0100

    wez again

--- .config/wezterm/windows.lua
+++ .config/wezterm/windows.lua
@@ -18,9 +18,11 @@ function module.apply(config)
     config.front_end = "WebGpu"
     config.webgpu_power_preference = "HighPerformance"
     config.webgpu_preferred_adapter = {
-        backend = "Dx12",
+        backend = "Vulkan",
         device = 39745,
         device_type = "IntegratedGpu",
+        driver = "Intel Corporation",
+        driver_info = "Intel driver",
         name = "Intel(R) UHD Graphics",
         vendor = 32902,
     }
