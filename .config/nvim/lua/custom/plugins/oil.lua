f296d539cfb8b07b0c8a3a64bc76f9973ba294fc
mbromell <code@mamo.aleeas.com>
2024-06-05 11:56:31 +0000

    Oil.nvim settings

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -1,3 +1,14 @@
+local hide_exact = {
+  ".",
+  "..",
+  ".git",
+  ".DS_Store",
+  ".Trash",
+  ".vscode",
+  ".idea",
+}
+local hide_pattern = {}
+
 return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -5,4 +16,27 @@ return {
     { "<leader>t", "<cmd>Oil<cr>", "Open file tree" },
   },
   opts = {},
+  init = function()
+    require("oil").setup({
+      view_options = {
+        show_hidden = true,
+        ---@diagnostic disable-next-line: unused-local
+        is_always_hidden = function(name, bufnr)
+          for _, value in ipairs(hide_exact) do
+            if name == value then
+              return true
+            end
+          end
+
+          for _, pattern in ipairs(hide_pattern) do
+            if name:match(pattern) then
+              return true
+            end
+          end
+
+          return false
+        end,
+      },
+    })
+  end,
 }
