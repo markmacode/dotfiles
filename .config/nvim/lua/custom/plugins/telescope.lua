26b3cdbb6bf4995de23941bfa95279eee88a3940
mbromell <code@mamo.aleeas.com>
2024-06-20 23:16:09 +0100

    Combining separated files

--- .config/nvim/lua/custom/plugins/telescope.lua
+++ .config/nvim/lua/custom/plugins/telescope.lua
@@ -41,6 +41,28 @@ return {
     }
   end,
   config = function()
-    require("custom.telescope")
+    local telescope = require("telescope")
+    telescope.load_extension("fzf")
+    telescope.setup({
+      pickers = {
+        git_files = {
+          show_untracked = true,
+        },
+      },
+      defaults = {
+        sorting_strategy = "ascending",
+        layout_strategy = "flex",
+        layout_config = {
+          prompt_position = "top",
+          -- For some reason builtin flex is not smart enough
+          flip_columns = 120,
+          flip_lines = 40,
+        },
+        file_ignore_patterns = {
+          ".DS_Store",
+          ".git/",
+        },
+      },
+    })
   end,
 }
