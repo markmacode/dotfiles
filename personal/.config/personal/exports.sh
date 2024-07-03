18e17369de6e95e8107db5ef58900c69d8773600
mbromell <code@mamo.aleeas.com>
2024-07-03 13:10:42 +0000

    Updating fzf

--- personal/.config/personal/exports.sh
+++ personal/.config/personal/exports.sh
@@ -11,10 +11,27 @@ export EDITOR="nvim"
 export PAGER="bat"
 
 export FZF_DEFAULT_COMMAND='fd -t file -LH -E "{\.git,node_modules}"'
-export FZF_DEFAULT_OPTS=" \
---color=bg+:#3c3836,bg:#32302f,spinner:#8ec07c,hl:#83a598 \
---color=fg:#bdae93,header:#83a598,info:#fabd2f,pointer:#8ec07c \
---color=marker:#8ec07c,fg+:#ebdbb2,prompt:#fabd2f,hl+:#838ba8"
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS \
+  --info=inline-right \
+  --ansi \
+  --border=none
+  --color=bg+:#2d3f76 \
+  --color=bg:#1e2030 \
+  --color=border:#589ed7 \
+  --color=fg:#c8d3f5 \
+  --color=gutter:#1e2030 \
+  --color=header:#ff966c \
+  --color=hl+:#65bcff \
+  --color=hl:#65bcff \
+  --color=info:#545c7e \
+  --color=marker:#ff007c \
+  --color=pointer:#ff007c \
+  --color=prompt:#65bcff \
+  --color=query:#c8d3f5:regular \
+  --color=scrollbar:#589ed7 \
+  --color=separator:#ff966c \
+  --color=spinner:#ff007c \
+"
 
 export NVM_DIR="$HOME/.nvm"
 
