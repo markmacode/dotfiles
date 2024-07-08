b4b702d58aa095917fda3d22804d10527e0dd9c4
mbromell <code@mamo.aleeas.com>
2024-07-08 17:29:34 +0100

    Giving wezterm zen mode

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,7 +4,8 @@ local config = wezterm.config_builder()
 
 -- Font stuff
 local font = "JetBrainsMono Nerd Font"
-config.font_size = 16
+local font_size = 16
+config.font_size = font_size
 config.font = wezterm.font({
   family = font,
   harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
@@ -33,6 +34,25 @@ config.font_rules = {
   },
 }
 config.line_height = 1
+
+config.keys = {
+  {
+    key = "j",
+    mods = "CMD|ALT|CTRL",
+    action = wezterm.action_callback(function(window, pane)
+      local resolution = 1440
+      local zoom_size = resolution / 45 -- this will be 32 on 1440p
+      local current_overrides = window:get_config_overrides()
+      if current_overrides ~= nil and current_overrides.font_size == zoom_size then
+        window:toggle_fullscreen()
+        window:set_config_overrides({ font_size = font_size })
+        return
+      end
+      window:toggle_fullscreen()
+      window:set_config_overrides({ font_size = zoom_size })
+    end),
+  },
+}
 
 -- Window stuff
 config.color_scheme = "Tokyo Night"
