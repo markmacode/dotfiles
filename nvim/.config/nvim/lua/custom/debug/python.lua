cacc2eb3d57e382de1bca1d5d57ccb13ad04367e
markmacode <code@mamo.aleeas.com>
2026-01-05 23:28:58 +0000

    Fix python dap

--- nvim/.config/nvim/lua/custom/debug/python.lua
+++ nvim/.config/nvim/lua/custom/debug/python.lua
@@ -1,6 +1,6 @@
 local dap = require("dap")
 local dap_py = require("dap-python")
-dap_py.setup()
+dap_py.setup("uv")
 
 -- More options, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings
 -- Using .vscode/launch.json might be a better approach on a per-project basis
